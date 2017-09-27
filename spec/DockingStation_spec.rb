8d0b65bc2b3d51a5336e048b77087abdb5224ee0
Christine <splogy2001@hotmail.com>
2017-09-27 17:11:04 +0100

    challenge 10

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -8,4 +8,15 @@ describe DockingStation do
       it 'when release_bike method is called, it creates an instance of Bike class' do
         expect(subject.release_bike).to be_an_instance_of(Bike)
       end
+        it 'when we call method working? on an instance of bike it answers true' do
+        bike = subject.release_bike
+        expect(bike.working?).to be true
+      end
+
 end
+
+#describe Bike do
+#    it 'when we call method working? on an instance of bike it answers true' do
+#      expect(subject.working?).to be true
+#    end
+#  end
