fa19d29af2abe1da05a266e6d246cc84c2144576
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-28 16:02:30 +0100

    Enabled docking stations to accept up to 20 bikes

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,17 +1,19 @@
 require_relative 'Bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    bike_to_release = @bike
-    @bike = nil
-    bike_to_release
+    raise 'No bikes available' if @bikes.length <= 0
+    @bikes.shift
   end
 
   def dock(bike)
-      fail 'At capacity' if @bike
-    @bike = bike
+    raise 'At capacity' if @bikes.length >= 20
+    @bikes << bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,28 +1,27 @@
-require "DockingStation"
+require 'DockingStation'
 
 describe DockingStation do
+  it 'when release_bike method is called, it returns an instance of Bike class' do
+    subject.dock Bike.new
+    expect(subject.release_bike).to be_an_instance_of(Bike)
+  end
 
-      it 'responds to release bike' do
-        expect(subject).to respond_to :release_bike
-      end
-      it 'when release_bike method is called, it creates an instance of Bike class' do
-        subject.dock Bike.new
-        expect(subject.release_bike).to be_an_instance_of(Bike)
-      end
+  it 'is possible to dock a bike to the DockingStation' do
+    bike = Bike.new
+    subject.dock bike
+    expect(subject.bikes[0]).to eq(bike)
+  end
 
-      it 'is possible to dock a bike to the DockingStation' do
-        bike = Bike.new
-        subject.dock bike
-        expect(subject.bike).to eq(bike)
-      end
+  it 'raises an error when you try to get a bike from it but there are none available' do
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
 
-      it 'raises an error when you try to get a bike from it but there are none available' do
-        expect{subject.release_bike}.to raise_error 'No bikes available'
-      end
-
-      it 'raises an error when you try to dock a bike when it has reached capacity' do
-        subject.dock Bike.new
-        expect{subject.dock Bike.new}.to raise_error 'At capacity'
-      end
-
+  it 'raises an error when you try to dock a bike when it has reached capacity' do
+    20.times { subject.dock Bike.new }
+    expect { subject.dock Bike.new }.to raise_error 'At capacity'
+  end
+  #
+  # it 'has a default capacity of 20 bikes' do
+  #   expect(subject.DEFAULT_CAPACITY).to eq 20
+  # end
 end
